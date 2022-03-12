b4e58538339f9c5b3e316c80216031610f54ec2e
Yi Zhang Lim (yil672) <yil672@mail.usask.ca>
2022-03-11 18:30:32 -0600

    staff methods added

--- src/Admin.java
+++ src/Admin.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 public class Admin extends Staff implements AdminInterface{
-
+    private ArrayList<Staff> staffList;
     public Admin(int id, String name, String job) {
         super(id, name, job);
+        staffList = new ArrayList<>();
     }
 
     @Override
@@ -11,17 +13,21 @@ public class Admin extends Staff implements AdminInterface{
 
     @Override
     public void viewStaffList() {
-
+        for (int x=0;x<staffList.size();x++) {
+            System.out.println(staffList.get(x).id+"\t"+staffList.get(x).name+"\t"+staffList.get(x).jobName);
+        }
     }
 
     @Override
     public void addStaff() {
-
+        staffList.add(new Staff(newID, newName, newJob));
     }
 
     @Override
     public void removeStaff() {
-
+        if (staffList.contains(staff)) {
+            staffList.remove(staff);
+        }
     }
 
     @Override

68864e60dd530230e98c2b5e5b01d4c2bb745ed8
Umer_Khan <umer.khan@usask.ca>
2022-03-17 17:51:50 -0600

    Implemented accessors and addFlight method for the Runway class.

--- src/Runway.java
+++ src/Runway.java
@@ -1,12 +1,28 @@
 import java.util.ArrayList;
 
 public class Runway {
-    public int id;
-    public ArrayList<Flight> queue;
+    private int id;
+    private ArrayList<Flight> queue;
 
     public Runway (int id) {
         this.id = id;
-        this.queue = new ArrayList<>();
+        this.queue = new ArrayList<Flight>();
+    }
+
+    // Accessors
+    public int getId() {return id;}
+    public ArrayList<Flight> getQueue() {return queue;}
+
+    public void addFlight(Flight flight) {
+        if (flight == null) {
+            throw new RuntimeException("Error: Flight to be added is invalid.");
+        }
+        else if (queue.contains(flight)) {
+            throw new RuntimeException("Error: The flight is already added to the runway queue.");
+        }
+        else {
+            this.queue.add(flight);
+        }
     }
 
     public static void main(String[] args) {}

b7e338233fbfe277abb2375a0761120932a72fb1
Umer_Khan <umer.khan@usask.ca>
2022-03-17 18:21:13 -0600

    Wrote test cases for Runway class

--- src/Runway.java
+++ src/Runway.java
@@ -25,5 +25,39 @@ public class Runway {
         }
     }
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        // Unit Tests
+        Runway runway = new Runway(1);
+        // Test getId()
+        if (runway.getId() != 1){
+            System.out.println("Error in getId(): Expected 1 but got " + runway.getId());
+        }
+        // Test addFlight()
+        // With valid input
+        Flight flight1 = new Flight(1, "Saskatoon", "Toronto", 25, 130);
+        try {
+            runway.addFlight(flight1);
+        }
+        catch (RuntimeException exception) {
+            System.out.println("Error in addFlight(): Exception thrown for valid input.");
+        }
+        // With invalid input
+        try {
+            runway.addFlight(null);
+            System.out.println("Error in addFlight(): Exception not thrown for invalid input.");
+        }
+        catch (RuntimeException exception) {
+            // Expected
+        }
+        // Test getQueue
+        Flight flight2 = new Flight(2, "Toronto", "Paris", 330, 1030);
+        runway.addFlight(flight2);
+        ArrayList<Flight> expected = new ArrayList<>();
+        expected.add(flight1);
+        expected.add(flight2);
+        ArrayList<Flight> result = runway.getQueue();
+        if (!result.equals(expected)) {
+            System.out.println("Error in getQueue: Expected " + expected + " but got " + result);
+        }
+    }
 }
